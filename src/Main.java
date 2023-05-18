829942993b3feacae0000f0406d9f7b045b956cb
DavidNebot94 <informaticadavidnebot@gmail.com>
2023-05-18 09:04:41 +0200

    Apply changes

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+package aplicacion8.pkg12;
+
+/**
+ *
+ * @author Ana
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
